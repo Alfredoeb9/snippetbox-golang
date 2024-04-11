84328280a59803e42094b0cbc7d87276bb479e43
Alfredoeb9 <alfredoeb96@gmail.com>
2024-04-11 00:14:48 -0700

    [headers] updated headers to include 201 on create and server on home request

--- main.go
+++ main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
+	"fmt" // New import
 	"log"
 	"net/http"
+	"strconv" // New import
 )
 
-// Define a home handler aka (controller) function which writes a byte slice containing
-// "Hello from Snippetbox" as the response body.
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from snippetbox"))
-}
 
 func main() {
 	// Use the http.NewServeMux() function to initialize a new servemux, then
     // register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("GET /{$}", home)
+    mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+    mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
 	// Print a log message to say that the server is starting
 	log.Print("starting server on :4000")
@@ -27,4 +27,46 @@ func main() {
 	// that any error returned by http.ListenAndServe() is always non-nul.
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+}
+
+
+// Define a home handler aka (controller) function which writes a byte slice containing
+// "Hello from Snippetbox" as the response body.
+func home(w http.ResponseWriter, r *http.Request) {
+	// Use the Header().Add() method to add a 'Server: Go' header to the 
+	// response header map. The first parameter is the header name, and
+	// the second parameter is the header value
+	w.Header().Add("Server", "Go")
+    w.Write([]byte("Hello from Snippetbox"))
+}
+
+// Add a snippetView handler function
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	
+    id, err := strconv.Atoi(r.PathValue("id"))
+	
+    if err != nil || id < 1 {
+		
+        http.NotFound(w, r)
+        return
+    }
+
+	
+
+	// This will allow us to interpolate the wildcard id value in our response body message 
+	// and write the response in a single line, like so:
+
+    fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+
+}
+
+// Add a snippetCreate handler function
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet"))
+}
+
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	// Use the w.WriteHeader() method to send a 201 status code
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Save a new snippet..."))
 }
